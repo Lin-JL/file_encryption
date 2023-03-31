4cfddf8b0bd4a4c3a86a0b970d0063705492f207
Lin-JL <1006480577@qq.com>
2023-04-01 00:04:44 +0800

    增加扩展名

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 from helper.func import get_filepath, get_password, get_operation
 from helper.secret import encrypt_dir, decrypt_dir
 
+extension_name = '.myenc'
+
 
 while True:
     file_path = get_filepath()
@@ -12,12 +14,16 @@ while True:
 
     dirname = os.path.dirname(file_path)
     if choice == 'e':
-        filename = '加密_' + os.path.basename(file_path)
+        filename = os.path.basename(file_path) + extension_name
         output_file_path = os.path.join(dirname, filename)
         encrypt_dir(file_path, password, output_file_path)
         print('加密完成\n')
     else:
-        filename = '解密_' + os.path.basename(file_path)
+        filename = os.path.basename(file_path)
+        if filename[-len(extension_name):] == extension_name:
+            filename = filename[:-len(extension_name)]
+        else:
+            filename = '解密_' + filename
         output_file_path = os.path.join(dirname, filename)
         decrypt_dir(file_path, password, output_file_path)
         print('解密完成\n')
